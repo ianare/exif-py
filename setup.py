0831404be6af6dacb1363a42411d96b52d436091
ianaré sévi <ianare@gmail.com>
2013-07-26 21:39:45 +0200

    improve package information

--- setup.py
+++ setup.py
@@ -4,14 +4,23 @@
 from distutils.core import setup
 
 setup(
-    name='ExifRead',
-    version='1.3.0',
-    author='Ianaré Sévi',
-    author_email='ianare@gmail.com',
-    packages=['exifread'],
-    scripts=['EXIF.py'],
-    url='http://pypi.python.org/pypi/ExifRead/',
-    license='BSD',
-    description='Extract Exif data from tiff and jpeg files.',
-    long_description='Easy to use library to extract Exif data from tiff and jpeg files.'
+    name="ExifRead",
+    version="1.3.0",
+    author="Ianaré Sévi",
+    author_email="ianare@gmail.com",
+    packages=["exifread"],
+    scripts=["EXIF.py"],
+    url="https://github.com/ianare/exif-py",
+    license="BSD",
+    keywords="exif image metadata photo",
+    description="Read Exif metadata from tiff and jpeg files.",
+    long_description="Easy to use Python module to extract Exif metadata from tiff and jpeg files.",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        ],
+    )
 )
