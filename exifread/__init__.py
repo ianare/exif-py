0107e66510ef92b067f49cafd538f6ad8f488ff3
ianaré sévi <ianare@gmail.com>
2014-11-22 18:53:33 +0100

    fix bug where exif info was processed twice

--- exifread/__init__.py
+++ exifread/__init__.py
@@ -202,12 +202,12 @@ def process_file(f, stop_tag=DEFAULT_STOP_TAG, details=True, strict=False, debug
             ifd_name = 'IFD %d' % ctr
         logger.debug('IFD %d (%s) at offset %d:', ctr, ifd_name, ifd)
         hdr.dump_ifd(ifd, ifd_name, stop_tag=stop_tag)
-        # EXIF IFD
-        exif_off = hdr.tags.get('Image ExifOffset')
-        if exif_off:
-            logger.debug(' Exif SubIFD at offset %d:', exif_off.values[0])
-            hdr.dump_ifd(exif_off.values[0], 'EXIF', stop_tag=stop_tag)
         ctr += 1
+    # EXIF IFD
+    exif_off = hdr.tags.get('Image ExifOffset')
+    if exif_off:
+        logger.debug('Exif SubIFD at offset %d:', exif_off.values[0])
+        hdr.dump_ifd(exif_off.values[0], 'EXIF', stop_tag=stop_tag)
 
     # deal with MakerNote contained in EXIF IFD
     # (Some apps use MakerNote tags but do not use a format for which we
