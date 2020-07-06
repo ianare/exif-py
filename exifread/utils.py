d619a48bc14e8a32f6487bee08ea9ec807692577
Grzegorz Ruciński <grucin@mobile4.pro>
2020-07-06 12:34:04 +0200

    Fix for user comment processing

--- exifread/utils.py
+++ exifread/utils.py
@@ -2,6 +2,11 @@
 Misc utilities.
 """
 
+try:
+    basestring
+except NameError:
+    basestring = str
+
 from fractions import Fraction
 
 
@@ -34,7 +39,8 @@ def make_string_uc(seq):
     Special version to deal with the code in the first 8 bytes of a user comment.
     First 8 bytes gives coding system e.g. ASCII vs. JIS vs Unicode.
     """
-    seq = seq[8:]
+    if not isinstance(seq, basestring):
+        seq = seq[8:]
     # Of course, this is only correct if ASCII, and the standard explicitly
     # allows JIS and Unicode.
     return make_string(seq)
