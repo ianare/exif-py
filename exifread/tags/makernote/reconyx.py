ab53ec5fe996d7302a4511601c4e45bf261ab0e3
Bryan Kappa <parappathekappa@gmail.com>
2020-07-18 23:45:18 -0700

    type 2 and type 3 tags

--- exifread/tags/makernote/reconyx.py
+++ exifread/tags/makernote/reconyx.py
@@ -11,11 +11,15 @@ TAGS_MAIN = {
 # maker notes for Reconyx UltraFire cameras (ref PH)
 # ported from https://fossies.org/linux/Image-ExifTool/lib/Image/ExifTool/Reconyx.pm
 TAGS_TYPE_2 = {
-    
+    0x18: ('FirmwareVersion', ),
+    0x1f: ('Micro1Version', ),
+    0x26: ('BootLoaderVersion', ),
+    0x2d: ('Micro2Version', ),
 }
 
 # maker notes for Reconyx HF2 PRO cameras (ref 3)
 # ported from https://fossies.org/linux/Image-ExifTool/lib/Image/ExifTool/Reconyx.pm
 TAGS_TYPE_3 = {
-    
+    0x10: ('FileNumber', ),
+    0x12: ('DirectoryNumber', ),
 }
