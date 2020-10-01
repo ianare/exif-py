c6167d72c5a0e4e87fc0c19833a671a5ea3954e7
Bryan Kappa <parappathekappa@gmail.com>
2020-10-01 11:37:54 -0700

    add sub-definitions to reconyx.py

--- exifread/tags/makernote/reconyx.py
+++ exifread/tags/makernote/reconyx.py
@@ -6,7 +6,12 @@ TAGS_HYPERFIRE = {
     0x00: ('MakerNoteVersion', make_string),  # Sometimes binary
     0x01: ('FirmwareVersion', ),
     0x04: ('FirmwareDate', ),
-    0x06: ('TriggerMode', ),
+    0x06: ('TriggerMode', {
+            'C': 'CodeLoc Not Entered',
+            'E': 'External Sensor',
+            'M': 'Motion Detection',
+            'T': 'Time Lapse'
+    }),
     0x07: ('Sequence', ),
     0x09: ('EventNumber', ),
     0x0b: ('DateTimeOriginal', ),
@@ -27,7 +32,10 @@ TAGS_HYPERFIRE = {
     0x25: ('Brightness', ),
     0x26: ('Sharpness', ),
     0x27: ('Saturation', ),
-    0x28: ('InfraredIlluminator', ),
+    0x28: ('InfraredIlluminator', {
+            0: 'Off',
+            1: 'On'
+    }),
     0x29: ('MotionSensitivity', ),
     0x2a: ('BatteryVoltage', ),
     0x2b: ('UserLabel', )
@@ -40,11 +48,23 @@ TAGS_ULTRAFIRE = {
     0x1f: ('Micro1Version', ),
     0x26: ('BootLoaderVersion', ),
     0x2d: ('Micro2Version', ),
-    0x34: ('TriggerMode', ),
+    0x34: ('TriggerMode', {
+            'M': 'Motion Detection',
+            'P': 'Point and Shoot',
+            'T': 'Time Lapse'
+    }),
     0x35: ('Sequence', ),
     0x37: ('EventNumber', ),
     0x3b: ('DateTimeOriginal', ),
-    0x42: ('DayOfWeek', ),
+    0x42: ('DayOfWeek', {
+            0: 'Sunday',
+            1: 'Monday',
+            2: 'Tuesday',
+            3: 'Wednesday',
+            4: 'Thursday',
+            5: 'Friday',
+            6: 'Saturday'
+    }),
     0x43: ('MoonPhase', {
             0: 'New',
             1: 'New Crescent',
@@ -57,7 +77,10 @@ TAGS_ULTRAFIRE = {
     }),
     0x44: ('AmbientTemperatureFahrenheit', ),
     0x46: ('AmbientTemperature', ),
-    0x48: ('Illumination', ),
+    0x48: ('Illumination', {
+            0: 'Off',
+            1: 'On'
+    }),
     0x49: ('BatteryVoltage', ),
     0x4b: ('SerialNumber', ),
     0x5a: ('UserLabel', )
@@ -70,11 +93,23 @@ TAGS_HF2PRO = {
     0x12: ('DirectoryNumber', ),
     0x2a: ('FirmwareVersion', ),
     0x30: ('FirmwareDate', ),
-    0x34: ('TriggerMode', ),
+    0x34: ('TriggerMode', {
+            'M': 'Motion Detection',
+            'P': 'Point and Shoot',
+            'T': 'Time Lapse'
+    }),
     0x36: ('Sequence', ),
     0x3a: ('EventNumber', ),
     0x3e: ('DateTimeOriginal', ),
-    0x4a: ('DayOfWeek', ),
+    0x4a: ('DayOfWeek', {
+            0: 'Sunday',
+            1: 'Monday',
+            2: 'Tuesday',
+            3: 'Wednesday',
+            4: 'Thursday',
+            5: 'Friday',
+            6: 'Saturday'
+    }),
     0x4c: ('MoonPhase', {
             0: 'New',
             1: 'New Crescent',
@@ -91,7 +126,10 @@ TAGS_HF2PRO = {
     0x54: ('Brightness', ),
     0x56: ('Sharpness', ),
     0x58: ('Saturation', ),
-    0x5a: ('Flash', ),
+    0x5a: ('Flash', {
+            0: 'Off',
+            1: 'On'
+    }),
     0x5c: ('AmbientInfrared', ),
     0x5e: ('AmbientLight', ),
     0x60: ('MotionSensitivity', ),
