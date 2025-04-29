2d403c12e6373f2f62d330cc3dffcb1be3b815b6
Ianaré Sévi <ianare@gmail.com>
2025-04-29 22:34:45 +0200

    add some Apple tags

--- exifread/tags/makernote/apple.py
+++ exifread/tags/makernote/apple.py
@@ -7,11 +7,47 @@ http://owl.phy.queensu.ca/~phil/exiftool/
 from typing import Dict, Tuple
 
 TAGS: Dict[int, Tuple] = {
+    0x0001: ("MakerNoteVersion",),
+    0x0004: (
+        "AEStable",
+        {
+            0: "No",
+            1: "Yes",
+        },
+    ),
+    0x0005: ("AETarget",),
+    0x0006: ("AEAverage",),
+    0x0007: (
+        "AFStable",
+        {
+            0: "No",
+            1: "Yes",
+        },
+    ),
     0x000A: (
         "HDRImageType",
         {
             3: "HDR Image",
             4: "Original Image",
+        },
+    ),
+    0x0014: (
+        "ImageCaptureType",
+        {
+            1: "ProRAW",
+            2: "Portrait",
+            10: "Photo",
+            11: "Manual Focus",
+            12: "Scene",
+        },
+    ),
+    0x0015: ("ImageUniqueID",),
+    0x002E: (
+        "CameraType",
+        {
+            0: "Back Wide Angle",
+            1: "Back Normal",
+            6: "Front",
         },
     ),
 }
